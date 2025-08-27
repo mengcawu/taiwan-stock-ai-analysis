41ea67315363fa7e9f7adf8d2f6a22228eb50cb2
mengcawu <mengcawu@gmail.com>
2025-08-26 19:18:12 -0700

    Add files via upload

---
+++ fetch_sample_data.py
@@ -0,0 +1,14 @@
+# 模擬測試資料
+def get_sample_data():
+    return {
+        "tech_stocks": [
+            {"name": "2330 台積電", "desc": "MACD 黃金交叉、突破月線"},
+            {"name": "2603 長榮", "desc": "RSI > 70、成交量放大"}
+        ],
+        "fund_stocks": [
+            {"name": "2454 聯發科", "desc": "EPS 連三季成長，ROE > 15%"}
+        ],
+        "chip_stocks": [
+            {"name": "2882 國泰金", "desc": "外資連五日買超，券商集中度高"}
+        ]
+    }
